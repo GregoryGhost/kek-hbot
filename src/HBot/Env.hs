8537a48edcc61b5d73e89f40dc5fefed1c032ad0
Gregory <lobster287@gmail.com>
2020-12-01 22:48:21 +0700

    Updated config deserialize

--- src/HBot/Env.hs
+++ src/HBot/Env.hs
@@ -1,72 +1,102 @@
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module HBot.Env
-    (
-        App(..),
-        Env(..),
-        Config(..),
-        load
-    )
+  ( App (..),
+    Env (..),
+    Config (..),
+    load,
+    telegramToken
+  )
 where
 
-import qualified Data.ByteString.Lazy as B
-import Control.Monad
-import Data.Text
-import Data.Typeable
-import Data.Aeson
-import GHC.Generics
 import Control.Applicative
-import Control.Monad.Reader
-import Control.Monad.IO.Class
+import Control.Monad
 import Control.Monad.Catch
+import Control.Monad.IO.Class
+import Control.Monad.Reader
+import Data.Aeson
+import qualified Data.ByteString.Lazy as B
+import Data.Text (unpack, Text)
+import Data.Typeable
+import GHC.Generics
 import HBot.Core.Logger
 import System.Directory
 import System.FilePath
 
-
 type App = ReaderT Env IO
 
-data Env = Env { 
-    config :: Config
-} deriving Show
+data Env
+  = Env
+      { config :: Config
+      }
+  deriving (Show)
 
-data Config = Config {
-    logLvl :: LogLvl
-    , logFile :: !Text
-} deriving (Show, Generic)
+data LogSettings
+  = LogSettings
+      { logLvl :: LogLvl,
+        logFile :: !Text
+      }
+  deriving (Show, Generic)
 
-newtype DecodeError = DecodeError String deriving Show
+data Config
+  = Config
+      { log :: LogSettings,
+        telegram :: TelegramSettings
+      }
+  deriving (Show, Generic)
+
+newtype DecodeError = DecodeError String deriving (Show)
 
 instance Exception DecodeError
 
+data TelegramSettings
+  = TelegramSettings
+      { token :: !Text
+      }
+  deriving (Show, Generic)
+
+telegramToken :: IO String
+telegramToken = do
+  env <- load
+  let tgToken = unpack $ token $ telegram $ config env
+  pure tgToken
 
 initEnv :: Reader Config Env
-initEnv = do 
-    config <- ask
-    let env = Env { config = config }
-
-    pure env
+initEnv = do
+  config <- ask
+  let env = Env {config = config}
+  pure env
 
 getEnv :: Config -> Env
 getEnv = runReader initEnv
 
 load :: (MonadIO m, MonadThrow m) => m Env
 load = do
-    currentDir <- liftIO $ getCurrentDirectory
-    let fileName = "config.json"
-    let byPath = currentDir </> fileName
-    let decodedConfig = (eitherDecodeFileStrict byPath) :: IO (Either String Config)
-    config <- decodeError =<< liftIO decodedConfig
-    let env = getEnv config
-
-    pure env
+  currentDir <- liftIO $ getCurrentDirectory
+  let fileName = "config.json"
+  let byPath = currentDir </> fileName
+  let decodedConfig = (eitherDecodeFileStrict byPath) :: IO (Either String Config)
+  config <- decodeError =<< liftIO decodedConfig
+  let env = getEnv config
+  pure env
 
 decodeError :: MonadThrow m => Either String a -> m a
 decodeError = either (throwM . DecodeError) pure
 
 instance FromJSON Config
+
 instance ToJSON Config
+
+instance FromJSON TelegramSettings
+
+instance ToJSON TelegramSettings
+
+instance FromJSON LogSettings
+
+instance ToJSON LogSettings
 
 --TODO: move to core module
 instance FromJSON LogLvl
+
 instance ToJSON LogLvl
