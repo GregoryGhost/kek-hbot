9e0531b95cf812cceae9c3ae2b2eb70edbe1df31
Gregory <lobster287@gmail.com>
2020-12-01 21:46:25 +0700

    Replace req on http conduit

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,40 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import HBot.Env as Env
 import Control.Monad.Except
 
+-- main :: IO ()
+-- main = do
+--     env <- Env.load
+--     print env
+--     runBot env `catchError` handle
+--     where
+--         runBot env = putStrLn "TODO"
+--         handle = undefined
+
+import qualified Data.ByteString.Lazy.Char8 as L8
+import           Network.HTTP.Simple
+import Control.Concurrent.Async
+
+-- main :: IO ()
+-- main = do
+--     response <- httpLBS "http://httpbin.org/get"
+
+--     putStrLn $ "The status code was: " ++
+--                show (getResponseStatusCode response)
+--     print $ getResponseHeader "Content-Type" response
+--     L8.putStrLn $ getResponseBody response
+
 main :: IO ()
 main = do
-    env <- Env.load
-    print env
-    runBot env `catchError` handle
-    where
-        runBot env = putStrLn "TODO"
-        handle = undefined
+  a1 <- async $ do httpLBS "http://www.haskell.org/"
+
+  a2 <- async $ do httpLBS "http://www.duckduckgo.com/"
+
+  page1 <- wait a1
+  page2 <- (\x -> "kek" ++ show x) <$> wait a2
+
+  print page1
+  print page2
